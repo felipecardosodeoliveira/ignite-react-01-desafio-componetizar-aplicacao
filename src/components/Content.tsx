7b4293a34c2c90c3378f54241f7313a550b9c90c
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2022-01-22 12:24:03 -0300

    Refatorando para que as props dos componentes Sidebar e Content não sejam do tipo (any)

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -11,18 +11,34 @@ interface MovieProps {
   Runtime: string;
 }
 
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
 
-export function Content(props: any) {
+interface ContentProps {
+  selectedGenre: GenreResponseProps,
+  movies: MovieProps[]
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
   return (
     <div className="container">
       <header>
-        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
       </header>
 
       <main>
         <div className="movies-list">
-          {props.movies.map((movie: MovieProps) => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,26 +1,32 @@
 import { Button } from './Button';
 
-interface Genre {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
+interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
 }
 
-export function SideBar(props: any) {
-  return (
-    <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
-      <div className="buttons-container">
-        {props.genres.map((genre: Genre) => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => props.handleClickButton(genre.id)}
-            selected={props.selectedGenreId === genre.id}
-          />
-        ))}
-      </div>
-    </nav>
-  )
+interface GenreProps {
+    genres: GenreResponseProps[];
+    selectedGenreId: number;
+    handleClickButton: Function;
+}
+
+export function SideBar({ genres, selectedGenreId, handleClickButton }: GenreProps) {
+    return (
+        <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+            <div className="buttons-container">
+                {genres.map((genre) => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => handleClickButton(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    )
 }
